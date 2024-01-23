c635aa2e18e64eb28731147287463959c4f33881
Popa Teodor <teodor.popa96@gmail.com>
2024-01-23 10:51:13 +0200

    Update

---
+++ task5.php
@@ -0,0 +1,15 @@
+<?php
+$customers = [5, 6, 3, 4, 3, 7, 9, 9, 12];
+
+$customers_sorted = $customers;
+sort($customers_sorted);
+
+// print var_dump($customers_sorted);
+
+$last = count($customers_sorted) -1;
+
+print "Youngest is $customers_sorted[0] years old.";
+
+print "Oldest is $customers_sorted[$last] years old.";
+
+
