eb541a11c27bf86fb5a93a54d42ec241ae72699e
Popa Teodor <teodor.popa96@gmail.com>
2024-01-31 16:12:12 +0200

    Done

---
+++ Tasks/task8.php
@@ -0,0 +1,20 @@
+<?php
+
+$countries_and_capitals = array(
+    "United States" => "Washington, D.C.",
+    "United Kingdom" => "London",
+    "France" => "Paris",
+    "Germany" => "Berlin",
+    "Italy" => "Rome",
+    "Canada" => "Ottawa",
+    "Japan" => "Tokyo",
+    "Australia" => "Canberra",
+    "Brazil" => "Brasília",
+    "India" => "New Delhi"
+);
+
+foreach ($countries_and_capitals as $x => $y) {
+    print "$x : $y \n";
+}
+
+
