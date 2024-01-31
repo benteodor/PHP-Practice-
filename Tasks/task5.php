3342deeecc98c9c57844dec485ef0ab7c70706dd
Popa Teodor <teodor.popa96@gmail.com>
2024-01-31 15:18:32 +0200

    Done

--- Tasks/task5.php
+++ Tasks/task5.php
@@ -2,10 +2,11 @@
 
 $number1 = 2;
 $number2 = 7; 
+
 $sum = 0;
 
-for ($i = $sum; $i <= $number2; $i++)
-{
-    $sum++;
+for ($i = $number1; $i <= $number2; $i++) {
+    $sum += $i;
 }
+ print $sum . " ";
 ?>
---
+++ Tasks/task6.php
@@ -0,0 +1,15 @@
+<?php 
+
+for($x=1; $x<=5; $x++)
+{
+    for($y=1; $y<$x; $y++)
+{
+print "*";
+if($y < $x)
+{
+    print " ";
+}
+}
+print "\n";
+}
+?>
