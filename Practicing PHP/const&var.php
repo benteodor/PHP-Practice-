184e3b234cba1936057ee7577d9e93cdf2f05887
Popa Teodor <teodor.popa96@gmail.com>
2024-02-28 11:55:57 +0200

    Checked

---
+++ Practicing PHP/const&var.php
@@ -0,0 +1,33 @@
+<?php
+
+/*$firstName = "Teo";
+$firstName = "Popa";
+
+echo $firstName;
+*/
+
+// Constants 
+
+// define("name", "value");
+
+//echo defined("STATUS_PAID");
+
+//define("STATUS_PAID", "paid");
+
+
+/*$paid = "PAID";
+
+define ("STATUS_" . $paid, $paid);
+
+echo STATUS_PAID;
+*/
+
+// echo PHP_VERSION;
+
+// Variable  Variables
+
+$foo = "bar";
+
+$$foo = "baz";
+
+echo $foo , ${$foo};
