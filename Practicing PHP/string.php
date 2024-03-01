94b3617777c845ed0b82531c86454b20094e8cb1
Popa Teodor <teodor.popa96@gmail.com>
2024-03-01 09:37:43 +0200

    update

---
+++ Practicing PHP/string.php
@@ -0,0 +1,17 @@
+<?php
+
+// Strings
+
+$firstName = "Teodor";
+// $lastName = "${firstName} Smith";
+
+$lastName = "Popa";
+
+$name = $firstName . " " . $lastName;
+
+echo $name;
+
+
+// Heredoc (you can have variables)
+
+// Nowdoc (you can't have variables)
